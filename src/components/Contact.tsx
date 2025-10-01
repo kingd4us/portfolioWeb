8e6aada52c1e7f20497490283ceeba27d09005be
Firdaus Manalu <fmanalu900@gmail.com>
2025-10-01 10:36:57 +0700

    Adding icon in the web title

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Mail, Linkedin, Send, CheckCircle } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
